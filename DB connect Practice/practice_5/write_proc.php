4713caf890fc9361b7b5278ea513baa9ed632395
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-04-21 17:56:22 +0900

    Update write_proc.php

--- DB connect Practice/practice_5/write_proc.php
+++ DB connect Practice/practice_5/write_proc.php
@@ -8,37 +8,24 @@
     $pw = password_hash($_POST['pw'], PASSWORD_DEFAULT);
     $title = $_POST['title'];
     $ctnt = $_POST['content'];
-    // $file = $_FILES['b_file'];
-    // $date = date('Y-m-d');
+    $file = $_FILES['b_file'];
+  
 
-    // if(($name && $pw && $title && $ctnt) != "") {
-        $sql = " INSERT INTO e_board
-            (name, pw, title, content)
+    $tmpfile =  $_FILES['b_file']['tmp_name'];
+    $o_name = $_FILES['b_file']['name'];
+    $filename = iconv("UTF-8", "EUC-KR",$_FILES['b_file']['name']);
+    $folder = "upload/".$filename;
+    move_uploaded_file($tmpfile,$folder);
+
+
+    $sql = " INSERT INTO e_board
+            (name, pw, title, content, file)
             VALUES
-            ('$name', '$pw', '$title', '$ctnt');
+            ('$name', '$pw', '$title', '$ctnt', '$folder');
             ";
-    // } else {
-    //     print "<script>alert('글쓰기에 실패했습니다.');
-    //     history.back();</script>";
-    // }
-
-    // $tmpfile =  $_FILES['b_file']['tmp_name'];
-    // $o_name = $_FILES['b_file']['name'];
-    // $filename = iconv("UTF-8", "EUC-KR",$_FILES['b_file']['name']);
-    // $folder = "upload/".$filename;
-    // $imageUpload = move_uploaded_file($tmpfile,$folder);
-
-    // if($imageUpload == true){
-    //     echo "파일이 정상적으로 업로드 되었습니다. <br>";
-    //     echo "<img src='{$folder}' width='100' />";
-    // }else{
-    //     echo "파일 업로드에 실패하였습니다.";
-    // }
 
     $result = mysqli_query($conn, $sql);
     mysqli_close($conn);
 
     header("Location: ./list.php");
 ?>
-
-
