b7d5c584ce45e43886409d21417a46ffe8d6eb19
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-05-02 17:40:48 +0900

    Update write.php

--- DB connect Practice/board_login/write.php
+++ DB connect Practice/board_login/write.php
@@ -3,29 +3,33 @@
     session_start();
     $login_user = $_SESSION["login_user"];
 
-?>
+    $writer = $login_user["i_user"];
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "stylesheet" type = "text/css" href = "common.css" />
-    <title>글쓰기</title>
-</head>
-<body>
-    <h1>글쓰기</h1>
-    <form action = "write_proc.php" method = "post" enctype="multipart/form-data">
-        <div><input type = "text" name = "title" placeholder="제목"></div>
-        <div><textarea name = "ctnt" placeholder="내용"></textarea></div>
-        <div id = "in_file">
-            <input type = "file" value="1" name="file" />
-        </div>
-        <div>
-            <input type = "submit" value = "글쓰기">
-            <input type = "reset" value = "초기화">
-        </div>
-    </form>
-</body>
-</html>
+    // t_board에 insert 완료 후 list.php로 이동
+
+    include_once "db/db_board.php";
+ 
+    $title = $_POST['title'];
+    $ctnt = $_POST['ctnt'];
+    $file = $_FILES['file'];
+
+
+    $tmpFile =  $_FILES['file']['tmp_name'];
+    // $o_name = $_FILES['file']['name'];
+    $fileName = iconv("UTF-8", "EUC-KR",$_FILES['file']['name']);
+    $folder = "upload/".$fileName;
+    move_uploaded_file($tmpFile,$folder);
+
+    $param = [
+            "writer" => $writer,
+            "title" => $title,
+            "ctnt" => $ctnt,
+            "fileName" => $fileName
+        ];
+    
+    $result = ins_board($param);
+
+
+    if($result) { //0빼고는 다 True
+        header("Location: list.php");
+    }
