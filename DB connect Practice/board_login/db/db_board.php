ac01a50d6f44f0c02b58401d57df158c8321eade
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-05-04 17:40:17 +0900

    Update db_board.php

--- DB connect Practice/board_login/db/db_board.php
+++ DB connect Practice/board_login/db/db_board.php
@@ -67,7 +67,9 @@
         $start_idx = $param['start_idx'];
         $row_count = $param['row_count'];
 
-        $sql = " SELECT A.i_board, A.title, date(A.created_at) as date, time(A.created_at) as time,
+        $sql = " SELECT A.i_board, A.title, 
+                        date(A.created_at) as date, time(A.created_at) as time,
+                        A.view,
                         B.nm
                  FROM t_board A
                  INNER JOIN t_user B 
@@ -112,7 +114,7 @@
         function sel_board(&$param) {
             $i_board = $param["i_board"];
 
-            $sql = "SELECT A.title, A.ctnt, A.created_at, A.file
+            $sql = "SELECT A.title, A.ctnt, A.created_at, A.file, A.view
                         , B.nm, B.i_user
                     FROM t_board A
                     INNER JOIN t_user B
@@ -210,4 +212,160 @@
             return $result;
         }
 
+        function search_board(&$param) {
+            $conn = get_conn();
+            $search_select = $param['search_select'];
+            $search_input_txt = $param['search_input_txt'];
+            $textArray = explode(" ", $search_input_txt); //검색어를 공백으로 나눈다
 
+            $where = []; //sql검색 시 열(속성) 이름
+            $sql = "SELECT A.i_board, A.title,date(A.created_at) as date, time(A.created_at) as time,
+                    B.i_user, B.nm
+                    FROM t_board A
+                    INNER JOIN t_user B 
+                    ON A.writer = B.i_user
+                    WHERE 
+                    ";
+
+            switch($search_select) {
+                case "search_writer":
+                    $where += ["B.nm"]; //i_user의 nm
+                    break;
+                case "search_title":
+                    $where += ["A.title"]; //t_board의 title
+                    break;
+                case "search_ctnt":
+                    $where += ["A.ctnt", "A.title"]; //t_board의 ctnt
+                    break;
+    
+            }
+
+            for ($i = 0; $i < count($textArray); $i++) {
+                for ($j = 0; $j < count($where); $j++) {
+                    $sql = $sql." $where[$j] LIKE '%{$textArray[$i]}%' ";
+
+                    //마지막 검색어가 아니라면
+                    if(($i !== count($textArray) - 1) || ($j !== count($where) -1)){ 
+                        $sql = $sql. "OR";
+                    }
+                }
+            }   
+ 
+            // for ($i = 0; $i < count($textArray); $i++) {
+            //     $sql = $sql." $where LIKE '%{$textArray[$i]}%' ";
+            //     if($i !== count($textArray) - 1) { //마지막 검색어가 아니면
+            //         $sql = $sql. "OR";
+            //     }
+            // }
+
+            //print $sql;
+
+            // echo $search_select."<br>";
+            // echo $search_input_txt."<br>";
+            // print_r($textArray);
+            
+            $result = mysqli_query($conn, $sql);
+            mysqli_close($conn);
+            return $result;
+
+        }
+
+        function visit() {
+            $conn = get_conn();
+
+            $YY = date('Y');
+            $MM = date('m');
+            $DD = date('d');
+
+            $dat = $YY."-".$MM."-".$DD;
+
+            $sql = " SELECT * FROM visit_db
+            WHERE redate = '$dat'
+            ";
+
+            $result = mysqli_query($conn, $sql);
+
+            $list = mysqli_num_rows($result);
+
+            if(!$list) { 
+                $count = 0; 
+            } else { 
+                $row = mysqli_fetch_array($result);
+                $count = $row['count'];
+            }
+
+            if($count === 0) {
+                $count++;
+                
+                $sql = "INSERT INTO visit_db
+                        VALUES ($count, '$dat')
+                        ";
+            } else {
+                $count++;
+                
+                $sql = "UPDATE visit_db
+                        SET count = $count
+                        WHERE redate = '$dat'
+                        ";
+            }
+        
+            mysqli_query($conn, $sql);
+            
+            $totalQurey = "SELECT SUM(count) FROM visit_db";
+            $totalCount = mysqli_fetch_array(mysqli_query($conn, $totalQurey))[0];
+            mysqli_close(($conn));
+            
+            return array($count, $totalCount);
+        }
+
+        function view(&$param) {
+            $conn = get_conn();
+            $i_board = $param['i_board'];
+
+            $sql = "UPDATE t_board
+                    SET view = view+1
+                    WHERE i_board = $i_board
+                    ";
+            $result = mysqli_query($conn, $sql);
+            mysqli_close($conn);
+
+            return $result;
+        }
+        
+        function comment(&$param) {
+            $conn = get_conn();
+
+            $i_board = $param['i_board'];
+
+            $sql = "SELECT A.*, B.nm
+                    FROM t_comment A
+                    INNER JOIN t_user B
+                    ON A.i_user = B.i_user
+                    WHERE A.i_board = '$i_board'
+                    ";
+            
+            $result = mysqli_query($conn, $sql);
+            mysqli_close($conn);
+
+            return $result;
+
+        }
+
+
+        function ins_comment(&$param){
+            $conn = get_conn();
+
+            $i_board = $param['i_board'];
+            $i_user = $param['i_user'];
+            $ctnt = $param['ctnt'];
+
+            $sql = "INSERT INTO t_comment
+                    (i_board, i_user, ctnt)
+                    VALUES 
+                    ('$i_board', '$i_user', '$ctnt')
+                    ";
+            print $sql;
+            $result = mysqli_query($conn, $sql);
+            mysqli_close($conn);
+            return $result;
+        }
