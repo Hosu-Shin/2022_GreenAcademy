edee5bc389c66806185ae8b4b598261c2049036e
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-06-21 15:39:07 +0900

    Update BoardController.php

--- PHP_MVC-main/application/controllers/BoardController.php
+++ PHP_MVC-main/application/controllers/BoardController.php
@@ -7,8 +7,27 @@
         public function list() {
             $model = new BoardModel();
             //$this->list = $model->selBoardList();
-            $this -> addAttribute("list", $model->selBoardList());
+            $this->addAttribute("title", "리스트");
+            $this->addAttribute("list", $model->selBoardList());
+            $this->addAttribute("js", ["board/list"]);
+
             return "board/list.php"; //view 파일명!
+        }
+
+        public function detail() {
+            $i_board = $_GET["i_board"];
+            
+            $model = new BoardModel();
+            $param = [
+                "i_board" => $i_board
+            ];
+
+            $this->addAttribute("data", $model->selBoard($param));
+            $this->addAttribute("js", ["board/detail"]);
+
+            return "board/detail.php";
+
+            //글번호, 제목, 내용, 글쓴이 이름, 작성일
         }
 
     }
