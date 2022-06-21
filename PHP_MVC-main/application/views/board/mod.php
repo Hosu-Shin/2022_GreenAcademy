75255bdf0f8cbec927ca96a91af0aedab91c1345
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-06-21 17:32:37 +0900

    Add files via upload

---
+++ PHP_MVC-main/application/views/board/mod.php
@@ -0,0 +1,10 @@
+<div>
+    <h1>수정</h1>
+    <form action="modProc.php" method="post">
+        <div>제목 : <input type="text" name="title" value="<?=$this->data->title?>"></div>
+        <div>내용 : <textarea name="ctnt"><?=$this->data->ctnt?></textarea></div>
+        <div>
+            <input type="submit" value="수정">
+        </div>
+    </form>
+</div>
