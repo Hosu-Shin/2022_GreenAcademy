0f1bd0585f49f6edc42eb34b73d5f6d102798451
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-06-21 16:48:56 +0900

    Update BoardController.php

--- PHP_MVC-main/application/controllers/BoardController.php
+++ PHP_MVC-main/application/controllers/BoardController.php
@@ -9,7 +9,7 @@
             //$this->list = $model->selBoardList();
             $this->addAttribute("title", "리스트");
             $this->addAttribute("list", $model->selBoardList());
-            $this->addAttribute("js", ["board/list"]);
+            $this->addAttribute("js", ["board/list"]); //파일을 여러개 보내야 할 경우가 있을 거기 때문에 배열로 만듬
 
             return "board/list.php"; //view 파일명!
         }
@@ -28,6 +28,19 @@
             return "board/detail.php";
 
             //글번호, 제목, 내용, 글쓴이 이름, 작성일
+        }
+
+        public function del() {
+            $i_board = $_GET["i_board"];
+
+            $model = new BoardModel();
+            $param = [
+                "i_board" => $i_board
+            ];
+
+            $this->addAttribute("data", $model->delBoard($param));
+
+            return "redirect:/board/list";
         }
 
     }
