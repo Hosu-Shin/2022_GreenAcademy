d04e2d0e7160d8ec6bd63af9d939166dd6d05585
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-06-22 17:44:57 +0900

    Update mod.php

--- PHP_MVC-main/application/views/board/mod.php
+++ PHP_MVC-main/application/views/board/mod.php
@@ -1,6 +1,7 @@
 <div>
     <h1>수정</h1>
-    <form action="modProc.php" method="post">
+    <form action="modProc" method="post">
+        <div><input type="hidden" name="i_board" value="<?=$this->data->i_board?>"></div>
         <div>제목 : <input type="text" name="title" value="<?=$this->data->title?>"></div>
         <div>내용 : <textarea name="ctnt"><?=$this->data->ctnt?></textarea></div>
         <div>
