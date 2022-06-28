56a6242a58ff0addf77754c103f546d289166dd8
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-06-28 16:48:39 +0900

    Update FeedController.php

--- PHPgram/application/controllers/FeedController.php
+++ PHPgram/application/controllers/FeedController.php
@@ -9,18 +9,44 @@ class FeedController extends Controller {
     }
 
     public function rest() {
-        print "method : " . getMethod();
+        //print "method : " . getMethod() . "<br>";
         switch(getMethod()) {
             case _POST:
-                print getIuser();
-               if(is_array($_FILES)) {
-                    foreach($_FILES['imgs']['name'] as $key => $value) {
-                        print "key : {$key}, value : {$value} <br>";
-                    }
+            if(!is_array($_FILES) || !isset($_FILES['imgs'])) {
+                return ["result" => 0];
+                //result가 0이면 실패, 1이어야 성공
+            }
+                
+            // $location = $_POST["location"]; 
+            // $ctnt = $_POST["ctnt"];
+            $iuser = getIuser();
+            
+            $param = [
+                "location" => $_POST["location"],
+                "ctnt" => $_POST["ctnt"],
+                "iuser" => $iuser
+            ];
+            $ifeed =  $this->model->insFeed($param);
+
+            foreach($_FILES["imgs"]["name"] as $key => $value) {
+                //확장자 얻어오기
+                $file_name = explode(".", $value);
+                
+                //배열의 마지막 인덱스 값 가져 오기 
+                //$file_name[count($file_name)-1];
+                $ext = end($file_name);
+
+                $saveDirectory = _IMG_PATH . "/profile/" . $iuser;
+                if(!is_dir($saveDirectory)) {
+                    mkdir($saveDirectory, 0777, true);
                 }
-                print "ctnt : " . $_POST['ctnt'] . "<br>";
-                print "location : " . $_POST['location'] . "<br>";
-                break;
+                $tempName = $_FILES['imgs']['tmp_name'][$key];
+                move_uploaded_file($tempName, $saveDirectory . "/test." . $ext); 
+                
+            }
+
+            return "redirect:/feed/index";
+               
         }
     }
 }
