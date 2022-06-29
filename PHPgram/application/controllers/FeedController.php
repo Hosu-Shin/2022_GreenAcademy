f64f1d1292f2adee7182fee9a6758622bba8b702
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-06-29 12:51:20 +0900

    Update FeedController.php

--- PHPgram/application/controllers/FeedController.php
+++ PHPgram/application/controllers/FeedController.php
@@ -12,50 +12,63 @@ class FeedController extends Controller {
         //print "method : " . getMethod() . "<br>";
         switch(getMethod()) {
             case _POST:
-            if(!is_array($_FILES) || !isset($_FILES['imgs'])) {
-                return ["result" => 0];
-                //result가 0이면 실패, 1이어야 성공
-            }
-                
-            // $location = $_POST["location"]; 
-            // $ctnt = $_POST["ctnt"];
-            $iuser = getIuser();
-            
-            $param = [
-                "location" => $_POST["location"],
-                "ctnt" => $_POST["ctnt"],
-                "iuser" => $iuser,
-            ];
-            $ifeed =  $this->model->insFeed($param);
-
-
-            foreach($_FILES["imgs"]["name"] as $key => $originFileNm) {
-            //확장자 얻어오기
-                //$file_name = explode(".", $originFileNm);
-                
-            //배열의 마지막 인덱스 값 가져 오기 
-                //$file_name[count($file_name)-1];
-                //$ext = end($file_name);
-
-                $saveDirectory = _IMG_PATH . "/feed/" . $ifeed;
-                if(!is_dir($saveDirectory)) {
-                    mkdir($saveDirectory, 0777, true);
+                if(!is_array($_FILES) || !isset($_FILES['imgs'])) {
+                    return ["result" => 0];
+                    //result가 0이면 실패, 1이어야 성공
                 }
-                $tempName = $_FILES['imgs']['tmp_name'][$key];
-                $randomFileNm = getRandomFile($originFileNm);
-                move_uploaded_file($tempName, $saveDirectory . "/" . $randomFileNm);
-
-                $paramImg = [
-                    "ifeed" => $ifeed,
-                    "img" => $randomFileNm
+                    
+                // $location = $_POST["location"]; 
+                // $ctnt = $_POST["ctnt"];
+                $iuser = getIuser();
+                
+                $param = [
+                    "location" => $_POST["location"],
+                    "ctnt" => $_POST["ctnt"],
+                    "iuser" => $iuser,
                 ];
-                
-                $this->model->insFeedImg($paramImg);
-                
-            }
+                $ifeed =  $this->model->insFeed($param);
 
-            return ["result" => $ifeed];
-               
+
+                foreach($_FILES["imgs"]["name"] as $key => $originFileNm) {
+                //확장자 얻어오기
+                    //$file_name = explode(".", $originFileNm);
+                    
+                //배열의 마지막 인덱스 값 가져 오기 
+                    //$file_name[count($file_name)-1];
+                    //$ext = end($file_name);
+
+                    $saveDirectory = _IMG_PATH . "/feed/" . $ifeed;
+                    if(!is_dir($saveDirectory)) {
+                        mkdir($saveDirectory, 0777, true);
+                    }
+                    $tempName = $_FILES['imgs']['tmp_name'][$key];
+                    $randomFileNm = getRandomFile($originFileNm);
+                    move_uploaded_file($tempName, $saveDirectory . "/" . $randomFileNm);
+
+                    $paramImg = [
+                        "ifeed" => $ifeed,
+                        "img" => $randomFileNm
+                    ];
+                    
+                    $this->model->insFeedImg($paramImg);
+                    
+                }
+
+                return ["result" => 1];
+
+            case _GET:
+                $page = 1;
+                if(isset($_GET["page"])) {
+                    $page = intval($_GET["page"]);
+                }
+                $startIdx = ($page-1) * _FEED_ITEM_CNT;
+                $param = [
+                    "startIdx" => $startIdx,
+                    "iuser" => getIuser()
+                ];
+                $list =  $this->model->selFeedList($param);
+                return $list;
+                               
         }
     }
 }
