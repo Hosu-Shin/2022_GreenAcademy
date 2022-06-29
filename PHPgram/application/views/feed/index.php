c7921922a098563728b060736e0739fba8c30aaf
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-06-29 16:40:18 +0900

    Update index.php

--- PHPgram/application/views/feed/index.php
+++ PHPgram/application/views/feed/index.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="d-flex flex-row justify-content-center w614">
+    <div class="d-flex flex-row justify-content-center">
         <div id="item_container" class="w614"></div>
     </div>
     <div class="loading d-none"><img src="/static/img/loading.gif" alt=""></div>
