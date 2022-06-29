de01f1279a1230fc528f53c4015aa45b5f1a622a
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-06-29 17:13:17 +0900

    Update common.js

--- PHPgram/static/js/common.js
+++ PHPgram/static/js/common.js
@@ -7,3 +7,30 @@ function encodeQueryString(params) {
                         ).join("&")
         : "";
 }
+/*
+    map: 길이가 같은 새로운 배열을 만들 때 사용
+    [key=params[key]] : 0번방 [page=1], 1번방 [limit=10]
+    join("&") : 0번방 1번방을 합치는데 구분자를 &로 사용
+    ?page=1&limit=10 이라는 결과값이 나온다
+*/
+
+
+function getDateTimeInfo(dt) {
+    const nowDt = new Date();
+    const targetDt = new Date(dt);
+
+    const nowDtSec = parseInt(nowDt.getTime() / 1000);
+    const targetDtSec = parseInt(targetDt.getTime() / 1000);
+
+    const diffSec = nowDtSec - targetDtSec;
+    if(diffSec < 120) {
+        return '1분 전';
+    } else if(diffSec < 3600) { //분 단위 (60 * 60)
+        return `${parseInt(diffSec / 60)}분 전`;
+    } else if(diffSec < 86400) { //시간 단위 (60 * 60 * 24)
+        return `${parseInt(diffSec / 3600)}시간 전`;
+    } else if(diffSec < 2592000) { //일 단위 (60 * 60 * 24 * 30)
+        return `${parseInt(diffSec / 86400)}일 전`;
+    }
+    return targetDt.toLocaleString();
+}
