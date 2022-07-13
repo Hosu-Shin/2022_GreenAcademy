1fbd563b372ca945e5bea32249fe02b50f26abce
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-07-13 18:06:19 +0900

    Add files via upload

---
+++ VueJS/vue-router-pjt/src/components/ProductItems.vue
@@ -0,0 +1,32 @@
+<template>
+    <td>{{ item.product_name }}</td>
+    <td>{{ numberComma(item.price) }}</td>
+    <td>{{ item.category }}</td>
+    <td>{{ item.delivery_price }}</td>
+
+</template>
+
+<script>
+export default {
+    props: {
+        item: Object
+    },
+    computed: {
+        getPrice() {
+            return this.numberComma(this.item.price);
+        },
+        getDeliveryPrice() {
+            return this.numberComma(this.item.delivery_price);
+        }
+    },
+    methods: {
+        numberComma(num) {
+            return String(num).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ VueJS/vue-router-pjt/src/views/EventClick.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <h1>이벤트 클릭</h1>
+        <button @click="increaseCnt(); printConsole()">Add 1</button>
+        <p>The counter is : {{ cnt }}</p>
+    </div>
+    <div>
+        {{ selectedValue }}
+        <select v-model="selectedValue" @change="changeSelect">
+            <option>서울</option>
+            <option>부산</option>
+            <option>제주</option>
+        </select>
+    </div>
+    <div>
+        {{ txtVal }}
+        <input type="txtVal" v-model="txtVal" @keyup="changetxtVal" @keyup.enter="enterTxtVal">
+        <!-- 값 넣는 동시에 출력하는 건 keyup -->
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            cnt: 0,
+            selectedValue: '',
+            txtVal: '',
+        }
+    },
+    methods: {
+        increaseCnt() {
+            this.cnt++;
+        },
+        printConsole() {
+            console.log(this.cnt);
+        },
+        changeSelect() {
+            alert(this.selectedValue);
+        },
+        changetxtVal() {
+            console.log(this.txtVal);
+        },
+        enterTxtVal() {
+            console.log('Enter!!!');
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
