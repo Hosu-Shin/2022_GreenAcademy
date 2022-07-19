69ba66a96d01af74271e9020c252b898884f6452
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-07-19 15:58:06 +0900

    제품 목록 프론트엔드

--- VueJS/SHOP/vue-shop-pjt/src/views/SalesList.vue
+++ VueJS/SHOP/vue-shop-pjt/src/views/SalesList.vue
@@ -1,12 +1,60 @@
 <template>
-<div>
-    <h1>SalesList</h1>
-     <router-link class="nav-link" to="/create">Registration</router-link>
+<div class="mt-3">
+    <div class="container">
+        <div class="float-end mb-1">
+            <router-link class="nav-link" to="/create">
+            <button type="button" class="btn btn-dark">
+                Registration
+            </button>
+            </router-link>
+        </div>
+        <table class="table table-bordered">
+            <thead>
+                <tr >
+                    <th></th>
+                    <th>Products</th>
+                    <th>Prices</th>
+                    <th>Shipping Costs</th>
+                    <th>Additional Charges</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr :key="name" v-for="(value, name) of ProductList">
+                    <td></td>
+                    <td>{{value.product_name}}</td>
+                    <td>{{value.product_price}}</td>
+                    <td>{{value.delivery_price}}</td>
+                    <td>{{value.add_delivery_price}}</td>
+                    <td>
+                        <button type="button" class="btn btn-info me-1">Image</button>
+                        <button type="button" class="btn btn-warning me-1">Edit</button>
+                        <button type="button" class="btn btn-danger me-1">Delete</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            ProductList: [],
+        }
+    },
+    created() {
+    this.getCategoryList();
+    },
+    methods: {
+        async getCategoryList() {
+            const productList = await this.$get('/api/productList2', {});
+            console.log(productList);
+            this.ProductList = productList;
+        }
+    }
 
 }
 </script>
