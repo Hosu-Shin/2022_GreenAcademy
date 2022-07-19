8e0968d73cb6ab41cee979728af0a6d5338940d5
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-07-19 13:02:26 +0900

    제품등록

--- VueJS/SHOP/PHPshop/application/controllers/ApiController.php
+++ VueJS/SHOP/PHPshop/application/controllers/ApiController.php
@@ -5,4 +5,10 @@
         public function categoryList() {
             return $this->model->getCategoryList();
         }
+
+        public function productInsert() {
+            $json = getJson();
+            print_r($json);
+            return [_RESULT => $this->model->productInsert($json)];
+        }
     }
