8b47241cb3d86bfe1cd2306ceab53f55772df570
Hosu-Shin <104125864+Hosu-Shin@users.noreply.github.com>
2022-07-20 11:33:41 +0900

    이미지 파일 등록하기

--- VueJS/SHOP/PHPshop/application/controllers/ApiController.php
+++ VueJS/SHOP/PHPshop/application/controllers/ApiController.php
@@ -27,4 +27,26 @@
             ];
             return $this->model->productDetail($param);
         }
+
+        public function upload() {
+            $urlPaths = getUrlPaths();
+            if(!isset($urlPaths[2]) || !isset($urlPaths[3])) {
+                exit();
+            }
+            $productId = intval($urlPaths[2]);
+            $type = intval($urlPaths[3]);
+            $json = getJson();
+            $image_parts = explode(";base64,", $json["image"]);
+            $image_type_aux = explode("image/", $image_parts[0]);
+            $image_type = $image_type_aux[1];
+            $image_base64 = base64_decode($image_parts[1]);
+            $dirPath = _IMG_PATH . "/" . $productId . "/" . $type;
+            $filePath = $dirPath . "/" . uniqid() . "." . $image_type;
+            if(!is_dir($dirPath)) {
+                mkdir($dirPath, 0777, true);
+            }
+            $result = file_put_contents($filePath, $image_base64);
+            
+            return [_RESULT => 1];
+        }
     }
